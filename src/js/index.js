3140d263ddebb3fc7c1e5f833ab70a81b770a0a1
Andrey Muzyka <muzykalniy@MacBook-Air-muzykalniy.local>
2024-07-26 16:43:29 +0200

    commit sswiper

--- src/js/index.js
+++ src/js/index.js
@@ -5,3 +5,17 @@ mobileNav();
 // Закрытие крестика
 import tagline from "./modules/tagline.js";
 tagline();
+
+// import Swiper bundle with all modules installed
+import Swiper from "swiper/bundle";
+
+// import styles bundle
+import "swiper/css/bundle";
+const swiper = new Swiper(".swiper", {
+  slidesPerView: "auto",
+  spaceBetween: 24,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
