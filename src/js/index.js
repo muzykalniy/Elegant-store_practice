2e0da2a97ed14a51ac2a3041613470598cfa85c0
muzykalniy <andreimuzyka18@gmail.com>
2024-08-01 13:34:25 +0200

    timer done

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,16 @@
 // Мобильная навигация
 import mobileNav from "./modules/mobile-nav.js";
-mobileNav();
-
 // Закрытие крестика
 import tagline from "./modules/tagline.js";
+// Свайпер
+import swiperFunc from "./modules/swiper.js";
+// Таймер
+import timer from "./modules/timer.js";
+
+mobileNav();
+
 tagline();
 
-// import Swiper bundle with all modules installed
-import Swiper from "swiper/bundle";
+swiperFunc();
 
-// import styles bundle
-// import "swiper/css/bundle"; - отключаем чтобы переподключить файлы с node modules swiper.scss swiper-vars
-const swiper = new Swiper(".swiper", {
-  slidesPerView: "auto",
-  spaceBetween: 24,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
+timer();
